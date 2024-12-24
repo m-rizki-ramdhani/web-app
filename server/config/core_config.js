926a0da57fe2d0e4e02c95bfbd02836c3cf2c3b8
Bagas Kusuma <bagaspbg64@gmail.com>
2024-12-25 01:46:48 +0700

    add user models

--- server/config/core_config.js
+++ server/config/core_config.js
@@ -2,6 +2,13 @@ require('dotenv').config()
 
 const config = {
     PORT  : process.env.PORT | 3000,
+    DEVELOPMENT: {
+        USERNAME: process.env.DB_USER || "root",          
+        PASSWORD: process.env.DB_PASS || "",            
+        DATABASE: process.env.DB_NAME || "my_database",  
+        HOST    : process.env.DB_HOST || "localhost",        
+        DIALECT : process.env.DB_DIALECT || "mysql",        
+    }
 } 
 
 module.exports = config;
