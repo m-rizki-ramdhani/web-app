2461399fe902d8870a58ff0dbfaa511c3ac0135a
m-rizki-ramdhani <ccetsf@gmail.com>
2024-12-25 00:34:56 +0700

    add sequelizerc

--- server/database/db_config.js
+++ server/database/db_config.js
@@ -2,11 +2,11 @@ const Sequelize = require('sequelize')
 
 const config = {
     development: {
-      username: "root",          // Username MySQL
-      password: "",              // Password MySQL (kosongkan jika menggunakan XAMPP default)
-      database: "my_database",   // Nama database yang Anda buat di phpMyAdmin
-      host: "localhost",         // Host MySQL
-      dialect: "mysql",          // Jenis database yang digunakan (MySQL)
+      username: process.env.DB_USER | "root",          // Username MySQL
+      password: process.env.DB_PASS | "",              // Password MySQL (kosongkan jika menggunakan XAMPP default)
+      database: process.env.DB_NAME | "my_database",   // Nama database yang Anda buat di phpMyAdmin
+      host    : process.env.DB_HOST | "localhost",         // Host MySQL
+      dialect : process.env.DB_DIALECT | "mysql",          // Jenis database yang digunakan (MySQL)
     }
   };
   
